899003d3910ded4f3db02871624dd6f8fdc6ed7c
Alexis Argyris <alexis.argyris@gmail.com>
2016-10-27 17:17:25 +0300

    wip1

--- index.js
+++ index.js
@@ -1,70 +1,60 @@
-var creds = require('./creds.js').creds;
-var evernote = require('./base.js').Evernote;
+'use strict';
+
+let creds = require('./creds.js').creds;
+let Promise = require('bluebird');
+let evernote = require('./base.js').Evernote;
 evernote.Client = require('./client.js').Client;
-var authToken;
-var client;
-var noteStore;
-var notebookguid;
-var noteFilter;
-var notesMetadataResultSpec;
-var noteResults = [];
-var noteguid;
+let authToken;
+let client;
+let noteStore;
+// let notebookguid;
+// let noteFilter;
+// let notesMetadataResultSpec;
+// let noteguid;
 
 function tagsNotebook(notebookguid) {
   // get tags of notebook
-  return new Promise((resolve, reject) => {
-    noteStore.listTagsByNotebook(notebookguid, function (err, res) {
-      if (err) { reject(new Error(err)); } 
-      else { resolve(res); }
-    })
-  })
+  let prms = Promise.promisify(noteStore.listTagsByNotebook);
+  return prms(notebookguid)
 }
-function count() {
+function count(noteFilter) {
   // get count of notes of notebook
-  return new Promise((resolve, reject) => {
-    noteStore.findNoteCounts(noteFilter, false, function (err, res) {
-      if (err) { reject(new Error(err)); } 
-      else { resolve(res); }
-    })
-  })
+  let prms = Promise.promisify(noteStore.findNoteCounts);
+  return prms(noteFilter, false)
 }
-function listNote(offset, count) {
+function listNote(offset, count, noteFilter, notesMetadataResultSpec) {
   // get the guids of notes
-  return new Promise((resolve, reject) => {
-    noteStore.findNotesMetadata(noteFilter, offset, count, notesMetadataResultSpec, function (err, res) {
-      if (err) { reject(new Error(err)); } 
-      else { resolve(res); }
-    })
-  })
+  let prms = Promise.promisify(noteStore.findNotesMetadata);
+  return prms(noteFilter, offset, count, notesMetadataResultSpec)
 }
-function listMoreNotes(offset, count, callback) {
+function listMoreNotes(offset, count, noteFilter, notesMetadataResultSpec, noteResults) {
   // get as many guids of notes as evernote will return and continue until the end
-  listNote(offset, count)
+  return listNote(offset, count, noteFilter, notesMetadataResultSpec)
     .then(response => {
       // add new results to result set
-      for (var index = 0; index < response.notes.length; index++) {
+      for (let index = 0; index < response.notes.length; index++) {
         noteResults.push(response.notes[index]);
       }
       if (offset < count) {
         // there are still more notes, continue
         offset += response.notes.length;
-        listMoreNotes(offset, count, callback);
+        return listMoreNotes(offset, count, noteFilter, notesMetadataResultSpec, noteResults);
       } else {
-        callback(noteResults);
+        return noteResults;
       }
-    })
-    .catch(reason => { throw new Error(reason); });
+    });
 }
 function notes(notebookguid) {
   // get note guids of notebook
-  return new Promise((resolve, reject) => {
-    count()
-      .then(response => {
-        var count = response.notebookCounts[notebookguid];
-        listMoreNotes(0, count, resolve);
-      })
-      .catch(reason => reject(reason))
-  })
+  let notesMetadataResultSpec = new evernote.NotesMetadataResultSpec;
+  let noteFilter = new evernote.NoteFilter;
+  noteFilter.notebookGuid = notebookguid;
+  noteFilter.ascending = false;
+  noteFilter.order = 1;
+  let noteResults = [];
+
+  return count(noteFilter)
+    .then(response => listMoreNotes(0, response.notebookCounts[notebookguid], noteFilter, notesMetadataResultSpec, noteResults));
 }
 function notebook(notebookguid) {
   // get tags and note guids of notebook
@@ -76,29 +66,24 @@ function notebook(notebookguid) {
 }
 function tagsNote(noteguid) {
   // get tags of note
-  return new Promise((resolve, reject) => {
-    noteStore.getNoteTagNames(noteguid, function (err, res) {
-      if (err) { reject(new Error(err)); } 
-      else { resolve(res); }
-    })
-  })
+  let prms = Promise.promisify(noteStore.getNoteTagNames);
+  return prms(noteguid);
 }
 function selectpic(res) {
   // select a usable pic resource
-  var 
-    lsindex = 0,
-    lsvalue = 0;
-    usableResources = [];
+  let lsindex = 0;
+  let lsvalue = 0;
+  let usableResources = [];
 
   if (res.resources) {
     // find out which resources have a non-empty sourceUrl
-    for(var i = 0; i < res.resources.length; i++) {
+    for (let i = 0; i < res.resources.length; i++) {
       if (res.resources[i].attributes.sourceURL) usableResources.push(res.resources[i]);
     }
     if (usableResources.length > 0) {
       // select a resource using evernote's 'largest-smallest' algorithm
-      for(var i = 0; i < usableResources.length; i++) {
-        var temp = Math.min(usableResources[i].width, usableResources[i].height);
+      for (let i = 0; i < usableResources.length; i++) {
+        let temp = Math.min(usableResources[i].width, usableResources[i].height);
         if (temp === 0) { continue; }
         if (temp > lsvalue) {
           lsindex = i;
@@ -113,27 +98,24 @@ function selectpic(res) {
 }
 function notecontent(noteguid) {
   // get content of note
-  return new Promise((resolve, reject) => {
-    var withContent = false;
-    var withResourcesData = true;
-    var withResourcesRecognition = false;
-    var withResourcesAlternateData = false;
-
-    noteStore.getNote(noteguid, withContent, withResourcesData, withResourcesRecognition, withResourcesAlternateData, function (err, res) {
-      if (err) { reject(new Error(err)); } 
-      else {
-        resolve({
-          "guid": res.guid,
-          "title": res.title,
-          "created": res.created,
-          "updated": res.updated,
-          "size": res.contentLength,
-          "picUrl": selectpic(res),
-          "sourceUrl": res.attributes.sourceURL || ''
-        });
-      }
-    })
-  })
+  let withContent = false;
+  let withResourcesData = true;
+  let withResourcesRecognition = false;
+  let withResourcesAlternateData = false;
+  let prms = Promise.promisify(noteStore.getNote);
+  return prms(noteguid, withContent, withResourcesData, withResourcesRecognition, withResourcesAlternateData)
+    .then(response => {
+      let result = {
+        'guid': response.guid,
+        'title': response.title,
+        'created': response.created,
+        'updated': response.updated,
+        'size': response.contentLength,
+        'picUrl': selectpic(response),
+        'sourceUrl': response.attributes.sourceURL || ''
+      };
+      return result
+    });
 }
 function note(noteguid) {
   // get tags and content of note of notebook
@@ -149,17 +131,21 @@ exports.handler = (event, context, callback) => {
   if ((event === undefined) || (event.cmd === undefined) || (event.cmd === '')) {
     callback(new Error('Missing cmd parameter'))
   } else {
+    // init evernote api
     authToken = creds.token;
-    client = new evernote.Client({ token: authToken, sandbox: false });
+    client = new evernote.Client({
+      token: authToken,
+      sandbox: false
+    });
     noteStore = client.getNoteStore();
+    // main switch
     switch (event.cmd) {
     case 'getNotebooks':
       // get all notebooks of the authenticated user
-      noteStore.listNotebooks(authToken, (error, response) => {
-        context.callbackWaitsForEmptyEventLoop = false;
-        if (error) { callback(error) } 
-        else { callback(null, response) }
-      });
+      let prms = Promise.promisify(noteStore.listNotebooks);
+      prms(authToken)
+        .then(response => callback(null, response))
+        .catch(error => callback(error));
       break;
     case 'getNotebook':
       // get info on a notebook
@@ -167,15 +153,15 @@ exports.handler = (event, context, callback) => {
       if (event.notebookguid === undefined) {
         callback(new Error('Missing notebook guid parameter'))
       } else {
-        notebookguid = event.notebookguid;
-        notesMetadataResultSpec = new evernote.NotesMetadataResultSpec;
-        noteFilter = new evernote.NoteFilter;
-        noteFilter.notebookGuid = notebookguid;
-        noteFilter.ascending = false;
-        noteFilter.order = 1;
-        notebook(notebookguid)
-          .then(response => { if (callback !== undefined) callback(null, {tags: response[0], notes: response[1]}) }) 
-          .catch(reason => { if (callback !== undefined) callback(reason) })
+        // notebookguid = event.notebookguid;
+        // notesMetadataResultSpec = new evernote.NotesMetadataResultSpec;
+        // noteFilter = new evernote.NoteFilter;
+        // noteFilter.notebookGuid = notebookguid;
+        // noteFilter.ascending = false;
+        // noteFilter.order = 1;
+        notebook(event.notebookguid)
+          .then(response => callback(null, {tags: response[0], notes: response[1]})) 
+          .catch(error => callback(error));
       }
      break;
     case 'getNote':
@@ -184,13 +170,18 @@ exports.handler = (event, context, callback) => {
       if (event.noteguid === undefined) {
         callback(new Error('Missing notebook guid parameter'))
       } else {
-        noteguid = event.noteguid;
-        note(noteguid)
-          .then(response => { if (callback !== undefined) callback(null, {tags: response[0], note: response[1]}) }) 
-          .catch(reason => { if (callback !== undefined) callback(reason) })
+        // noteguid = event.noteguid;
+        note(event.noteguid)
+          .then(response => {debugger; callback(null, {tags: response[0], note: response[1]})}) 
+          .catch(error => {debugger; callback(error)});
       }
       break;
     default:
     }
   }
 }
+
+exports.handler({
+  'cmd': 'getNote',
+  'noteguid': '9166ce04-72a8-4321-b0d1-e8c83f84d223'
+});
